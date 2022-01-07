ff5dc27d6e4142dc25c4770f4c594016c8842e79
b08901152 <b08901152@ntu.edu.tw>
2022-01-07 20:57:12 +0800

    add file

---
+++ Video Streaming Using RTSP/ClientLauncher.py
@@ -0,0 +1,27 @@
+import sys, signal
+from tkinter import Tk
+from Client import Client
+
+def signal_handler(signal, frame):
+    print('\nGoodbye!')
+    sys.exit(0)
+
+if __name__ == "__main__":
+	# setup terminator
+	signal.signal(signal.SIGINT,signal_handler)
+
+	try:
+		serverAddr = sys.argv[1]
+		serverPort = sys.argv[2]
+		rtpPort = sys.argv[3]
+		fileName = sys.argv[4]	
+	except:
+		print("[Usage: ClientLauncher.py Server_name Server_port RTP_port Video_file]\n")
+	
+	#root = Tk()
+	
+	# Create a new client
+	app = Client(serverAddr, serverPort, rtpPort, fileName)
+	app.master.title("RTPClient")	
+	app.master.mainloop()
+	
